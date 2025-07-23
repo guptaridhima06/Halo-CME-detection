359a9e6b23dfdd33b88d15dde35889a7c6ff774d
guptaridhima06 <guptaridhima1106@gmail.com>
2025-07-23 20:53:14 +0530

    Create config.py

---
+++ src1/config.py
@@ -0,0 +1,8 @@
+# src1/config.py
+
+class ModelParameters:
+    def __init__(self):
+        self.particle_baseline_hours = 12
+        self.cooldown_hours = 6
+        self.high_energy_bin_start = 8
+        self.high_energy_threshold = 5.0
